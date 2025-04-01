b7212367f50bafac0b3d6e9d0a87585551b77cf2
MetalSnow <yasuotrend@gmail.com>
2025-04-01 10:48:15 +0800

    style: complete styling for form and messageDetail

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -1,37 +1,37 @@
-const { Router } = require('express');
-const { nanoid } = require('nanoid');
+const { Router } = require("express");
+const { nanoid } = require("nanoid");
 
 const indexRouter = Router();
 
 const messages = [
   {
     id: nanoid(),
-    text: 'Hello, im a Business man :D',
-    user: 'Metal',
+    text: "Hello, im a Business man :D",
+    user: "Aiden",
     added: new Date(),
   },
   {
     id: nanoid(),
-    text: 'Hello, im a software engineer',
-    user: 'Metal',
+    text: "Hello, im a software engineer",
+    user: "Metal",
     added: new Date(),
   },
   {
     id: nanoid(),
-    text: 'Hi, im an architect',
-    user: 'Bob',
+    text: "Hi, im an architect",
+    user: "Bob",
     added: new Date(),
   },
   {
     id: nanoid(),
-    text: 'hey, im a doctor',
-    user: 'Vincent',
+    text: "hey, im a doctor",
+    user: "Vincent",
     added: new Date(),
   },
 ];
 
-indexRouter.get('/', (req, res) => {
-  res.render('index', { title: 'Mini Messageboard', messages: messages });
+indexRouter.get("/", (req, res) => {
+  res.render("index", { title: "Mini Messageboard", messages: messages });
 });
 
 module.exports = { indexRouter, messages };
